45f8e96e1095d7ff3ceeabfc38b5f1e1d1177df1
Nick De Breuck <nick.debreuck@outlook.com>
2019-04-09 14:14:59 +0200

    updated the default shader

--- OpenGLAbstracted/Data/Shaders/src/DefaultVertexShader.glsl
+++ OpenGLAbstracted/Data/Shaders/src/DefaultVertexShader.glsl
@@ -3,17 +3,17 @@
 // The #version preprocessor directive is used to indicate that the code that follows i GLSL 1.50 code
 // using OpenGL's core profile.
 
-layout (location = 0) in vec2 position;
-layout (location = 1) in vec3 color;
-layout (location = 2) in vec2 texcoord;
+in vec2 position;
+in vec3 color;
+in vec2 texcoord;
 
 // The color to output to the fragment shader
 out vec3 Color;
 out vec2 Texcoord;
 
-/* uniform mat4 model;
+uniform mat4 model;
 uniform mat4 view;
-uniform mat4 proj; */
+uniform mat4 proj;
 
 // Apart from regular C types, GLSL has built-in vector and matrix types
 // identified by vec* and mat* identifiers.
@@ -34,7 +34,7 @@ void main()
 // because the position is needed for primitive assembly and many other built-in processes.
 // For these to function correctly, the last value w needs to have a value of 1.0f.
 // Other than that, you're free to do anything you want with the attributes.
-gl_Position =/*  proj * view * model *  */vec4(position, 0.0f, 1.0f);
+gl_Position = proj * view * model * vec4(position, 0.0f, 1.0f);
 Color = color;
 Texcoord = texcoord;
 };
